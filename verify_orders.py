fd440d49f496b7a2a2ca148e68847b6387a1ca6f
Ell <62902904+e-goldman@users.noreply.github.com>
2020-08-31 17:04:47 -0400

    Create verify_orders.py

---
+++ verify_orders.py
@@ -0,0 +1,21 @@
+def verify_orders(register1, register2, served_orders):
+	
+  # returns False if one or more placed orders was never completed, or one or more junk orders was completed
+	if len(served_orders) != len(register1) + len(register2):
+		return False
+	
+	r1_index = 0
+	r2_index = 0
+	
+  # returns False if an element in served_orders is not the "next" element in register1 or register2 
+  # else increments pointer for relevent register
+	for order in served_orders:
+		if r1_index < len(register1) and order == register1[r1_index]:
+			r1_index += 1
+		elif r2_index < len(register2) and order == register2[r2_index]:
+			r2_index += 1
+		else:
+			return False
+	
+  # returns True if no above failure conditions are found
+	return True
